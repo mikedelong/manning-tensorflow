e370df4b10eaee0f7bfa783bf75278fef1eb5e38
Mike DeLong <mikedelong@outlook.com>
2018-02-17 15:44:29 -0500

    feature: initial examples of list and numpy array tensors.

--- page30.py
+++ page30.py
@@ -1,1 +1,33 @@
+import logging
+import time
 
+import numpy as np
+import tensorflow as tf
+
+start_time = time.time()
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+matrix_one = [[1.0, 2.0], [3.0, 4.0]]
+logger.debug(type(matrix_one))
+tensor_one = tf.convert_to_tensor(matrix_one)
+logger.debug(type(tensor_one))
+
+matrix_two = np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)
+logger.debug(type(matrix_two))
+tensor_two = tf.convert_to_tensor(matrix_two)
+logger.debug(type(tensor_two))
+
+logger.debug('done')
+finish_time = time.time()
+elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
