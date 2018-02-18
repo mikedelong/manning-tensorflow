3ba157b5960f8e3609ff768e2c883b19a4bb68ed
Mike DeLong <mikedelong@outlook.com>
2018-02-18 17:00:25 -0500

    feature: define and run the tensor negation code, but use the interactive session instead

---
+++ page35.py
@@ -0,0 +1,31 @@
+import logging
+import time
+
+start_time = time.time()
+
+import tensorflow as tf
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+session = tf.InteractiveSession()
+tensor = tf.constant([[1.0, 2.0]])
+negative_tensor = tf.negative(tensor)
+logger.debug('tensor is %s and negative tensor is %s' % (tensor, negative_tensor))
+result = negative_tensor.eval()
+
+logger.debug('result: %s' % result)
+session.close()
+
+logger.debug('done')
+finish_time = time.time()
+elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
