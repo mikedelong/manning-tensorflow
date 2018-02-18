bfcfe62ab9f8f5547fc6ee2d396e2a4c0155e979
Mike DeLong <mikedelong@outlook.com>
2018-02-18 16:55:29 -0500

    feature: negative tensor defined but not run.

---
+++ page32.py
@@ -0,0 +1,26 @@
+import logging
+import time
+
+start_time = time.time()
+
+import tensorflow as tf
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+tensor = tf.constant([[1, 2]])
+negative_tensor = tf.negative(tensor)
+logger.debug('tensor is %s and negative tensor is %s' % (tensor, negative_tensor))
+
+logger.debug('done')
+finish_time = time.time()
+elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
