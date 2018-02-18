ee0cf673509be1493a8073946f02e275dedb4cf1
Mike DeLong <mikedelong@outlook.com>
2018-02-18 17:07:14 -0500

    feature: a simple example with a session log.

---
+++ page37.py
@@ -0,0 +1,30 @@
+import logging
+import time
+
+start_time = time.time()
+
+import tensorflow as tf
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+tensor = tf.constant([[1.0, 2.0]])
+negative_tensor = tf.negative(tensor)
+config = tf.ConfigProto(log_device_placement=True)
+with tf.Session(config=config) as session:
+    result = session.run(negative_tensor)
+
+logger.debug('result is %s' % result)
+
+logger.debug('done')
+finish_time = time.time()
+elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
