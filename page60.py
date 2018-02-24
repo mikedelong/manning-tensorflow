0906a7fe9fa93bc47ccf4ac1445f5644158eb56c
Mike DeLong <mikedelong@outlook.com>
2018-02-24 16:48:41 -0500

    feature: first complete working version

--- page60.py
+++ page60.py
@@ -1,7 +1,15 @@
 import logging
 import time
 
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
 start_time = time.time()
+
+
+def model(arg_x, arg_w):
+    return tf.multiply(arg_x, arg_w)
 
 formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
 logger = logging.getLogger('main')
@@ -11,6 +19,37 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
+
+random_seed = 60
+np.random.seed(random_seed)
+learning_rate = 0.01
+training_epochs = 100
+
+x_train = np.linspace(-1, 1, 101)
+y_train = 2 * x_train + np.random.randn(*x_train.shape) * 0.33
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+name = 'weights'
+w = tf.Variable(0.0, name=name)
+y_model = model(X, w)
+cost = tf.reduce_mean(tf.square(Y - y_model))
+train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+session = tf.Session()
+initializer = tf.global_variables_initializer()
+session.run(initializer)
+for epoch in range(training_epochs):
+    for (x_item, y_item) in zip(x_train, y_train):
+        session.run(train_op, feed_dict={X: x_item, Y: y_item})
+
+w_val = session.run(w)
+session.close()
+
+plt.scatter(x_train, y_train)
+y_learned = x_train * w_val
+plt.plot(x_train, y_learned, 'r')
+outfile = './page60.png'
+plt.savefig(outfile)
 
 logger.debug('done')
 finish_time = time.time()
