6a2e2d792441a39b1179c133d68742f923173f15
Mike DeLong <mikedelong@outlook.com>
2018-02-25 13:21:31 -0500

    feature: raw training data, not yet complete.

--- page63.py
+++ page63.py
@@ -1,6 +1,8 @@
 import logging
 import time
 
+import numpy as np
+
 start_time = time.time()
 
 formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
@@ -11,6 +13,21 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
+
+random_seed = 63
+np.random.seed(random_seed)
+learning_rate = 0.01
+training_epochs = 100
+
+trX = np.linspace(-1.0, 1.0, 101)
+
+coefficients_count = 6
+trY_coefficients = range(1, 7, 1)
+trY = 0
+for index in range(coefficients_count):
+    trY += trY_coefficients[index] + np.power(trX, index)
+
+trY += np.random.randn((len(trX),)) + 1.5
 
 logger.debug('done')
 finish_time = time.time()
