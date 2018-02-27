f069e073b07d1e180f8f68ad8f251a6227fa738c
mikedelong <mikedelong@outlook.com>
2018-02-27 12:51:30 -0500

    feature: added logging, progress logging, overall time.

--- page69.py
+++ page69.py
@@ -1,5 +1,8 @@
 import csv
+import logging
 import time
+
+start_time = time.time()
 
 
 def read(filename, date_idx, date_parse, year, bucket=7):
@@ -18,10 +21,25 @@ def read(filename, date_idx, date_parse, year, bucket=7):
             if t.tm_year == year and t.tm_yday < (days_in_year - 1):
                 result[int(t.tm_yday / bucket)] += 1
             count += 1
-            if count % 1000 == 0 and count > 0:
-                print(count)
+            if count % 10000 == 0 and count > 0:
+                logger.debug(count)
 
     return result
 
 
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
 freq = read('./input/311_Call_Center_Tracking_Data__Archived_.csv', 0, '%m/%d/%Y', 2014)
+
+logger.debug('done')
+finish_time = time.time()
+elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
