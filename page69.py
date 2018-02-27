c1b4dabfef86b6d3f4c250ce9e208ac88abf97ba
mikedelong <mikedelong@outlook.com>
2018-02-27 12:48:39 -0500

    feature: first working version; uses next() correctly, opens the input file as text (not binary) and logs progress using print(). Needs some work.

--- page69.py
+++ page69.py
@@ -4,17 +4,24 @@ import time
 
 def read(filename, date_idx, date_parse, year, bucket=7):
     days_in_year = 365
-    freq = {}
+    result = {}
     for period in range(0, int(days_in_year / bucket)):
-        freq[period] = 0
-    with open(filename, 'rb') as csvfile:
+        result[period] = 0
+    with open(filename, 'r') as csvfile:
         csvreader = csv.reader(csvfile)
-        csvreader.next()
+        next(csvreader)
+        count = 0
         for row in csvreader:
             if row[date_idx] == '':
                 continue
             t = time.strptime(row[date_idx], date_parse)
             if t.tm_year == year and t.tm_yday < (days_in_year - 1):
-                freq[int(t.tm_yday / bucket)] += 1
+                result[int(t.tm_yday / bucket)] += 1
+            count += 1
+            if count % 1000 == 0 and count > 0:
+                print(count)
 
-    return freq
+    return result
+
+
+freq = read('./input/311_Call_Center_Tracking_Data__Archived_.csv', 0, '%m/%d/%Y', 2014)
