96cf79dbde5728feab60896c68eb2a87b977c8e0
mikedelong <mikedelong@outlook.com>
2018-03-02 08:20:23 -0500

    feature: plot out our test data as a scatter plot.

--- page86.py
+++ page86.py
@@ -1,6 +1,7 @@
 import logging
 import time
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 start_time = time.time()
@@ -19,6 +20,15 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
+
+random_seed = 86
+x1 = np.random.normal(-4, 2, 1000)
+x2 = np.random.normal(4, 2, 1000)
+xs = np.append(x1, x2)
+ys = np.asarray([0.0] * len(x1) + [1.0] * len(x2))
+plt.scatter(xs, ys)
+scatter_file = './output/page86_scatter.png'
+plt.savefig(scatter_file)
 
 logger.debug('done')
 finish_time = time.time()
