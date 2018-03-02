80521b23317b16bb0ce6064d2992ea517d02c130
mikedelong <mikedelong@outlook.com>
2018-03-02 09:46:36 -0500

    feature: create the fake data.

--- page88.py
+++ page88.py
@@ -21,6 +21,21 @@ console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
 
 random_seed = 88
+np.random.seed(random_seed)
+learning_rate = 0.1
+epoch_count = 2000
+
+x1_label1 = np.random.normal(3, 1, 1000)
+x2_label1 = np.random.normal(2, 1, 1000)
+x1_label2 = np.random.normal(7, 1, 1000)
+x2_label2 = np.random.normal(6, 1, 1000)
+
+x1s = np.append(x1_label1, x1_label2)
+logger.debug('we have %d x1s' % x1s.size)
+x2s = np.append(x2_label1, x2_label2)
+logger.debug('we have %d x2s' % x2s.size)
+ys = np.asarray([0.0] * x1_label1.size + [1.0] * x1_label2.size)
+logger.debug('we have %d ys' % ys.size)
 
 logger.debug('done')
 finish_time = time.time()
