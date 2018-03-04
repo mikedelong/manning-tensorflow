7a41fd699171d538edbdcf0a263ae497e30b838b
Mike DeLong <mikedelong@outlook.com>
2018-03-04 16:00:11 -0500

    feature: we are feature-complete and can list the contents of the folder.

--- page101.py
+++ page101.py
@@ -1,6 +1,8 @@
 import logging
 import time
 
+import tensorflow as tf
+
 start_time = time.time()
 
 formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
@@ -11,6 +13,22 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
+
+filenames = tf.train.match_filenames_once('./input/audio_dataset/*.wav')
+filename_count = tf.size(filenames)
+filename_queue = tf.train.string_input_producer(filenames)
+reader = tf.WholeFileReader()
+file_name, file_contents = reader.read(filename_queue)
+
+with tf.Session() as session:
+    session.run(tf.global_variables_initializer())
+    file_count = session.run(filename_count)
+    coordinator = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coordinator)
+
+    for index in range(file_count):
+        audio_file = session.run(file_name)
+        logger.debug('file name: %s' % audio_file)
 
 logger.debug('done')
 finish_time = time.time()
