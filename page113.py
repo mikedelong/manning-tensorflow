9aaa8e7e1552dd215f93cbd2fcf4507029cfc9a7
mikedelong <mikedelong@outlook.com>
2018-03-07 08:32:49 -0500

    checkpoint: partly implemented.

--- page113.py
+++ page113.py
@@ -1,6 +1,9 @@
 import logging
 import time
 
+import numpy as np
+import tensorflow as tf
+
 start_time = time.time()
 
 formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
@@ -11,6 +14,45 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
+
+
+class SOM:
+    def __init__(self, width, height, dim):
+        self.num_iters = 100
+        self.width = width
+        self.height = height
+        self.dim = dim
+        self.node_locs = self.get_locs()
+
+        nodes = tf.Variable(tf.random_normal([width * height, dim]))
+        self.nodes = nodes
+
+        x = tf.placeholder(tf.float32, [dim])
+        iter = tf.placeholder(tf.float32)
+        self.x = x
+        self.iter = iter
+        bmu_loc = self.get_bmu_loc(x)
+        self.propagate_nodes = self.get_propagation(bmu_loc, x, iter)
+
+    def get_propagation(self, bmu_loc, x, iter):
+        num_nodes = self.width * self.height
+        rate = 1.0 - tf.div(iter, self.num_iters)
+        alpha = 0.5 * rate
+        sigma = rate * tf.to_float(tf.maximum(self.width, self.height)) / 2.0
+        expanded_bmu_loc = tf.expand_dims(tf.to_float(bmu_loc), 0.0)
+        sqr_dists_from_bmu = tf.reduce_sum(tf.square(tf.subtract(expanded_bmu_loc, self.node_locs)), 1)
+        neigh_factor = tf.exp(-tf.div(sqr_dists_from_bmu, 2 * tf.square(sigma)))
+        rate = tf.multiply(alpha, neigh_factor)
+        rate_factor = tf.stack([tf.tile(tf.slice(rate, [i], [1]), [self.dim]) for i in range(num_nodes)])
+        nodes_diff = tf.multiply(rate_factor, tf.subtract(tf.stack([x for i in range(num_nodes)]), self.nodes))
+        update_nodes = tf.add(self.nodes, nodes_diff)
+        result = tf.assign(self.nodes, update_nodes)
+        return result
+
+
+random_seed = 113
+np.random.seed(random_seed)
+tf.set_random_seed(random_seed)
 
 logger.debug('done')
 finish_time = time.time()
