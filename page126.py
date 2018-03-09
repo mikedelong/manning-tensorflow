6191924a7f62ca6f29941f19a22c667bf32ac838
mikedelong <mikedelong@outlook.com>
2018-03-09 14:33:34 -0500

    feature: added the important imports feature: added the beginnings of the HMM class

--- page126.py
+++ page126.py
@@ -1,7 +1,18 @@
 import logging
 import time
 
+import numpy as np
+import tensorflow as tf
+
 start_time = time.time()
+
+
+class HMM(object):
+    def __init__(self, initial_prob, trans_prob, obs_prob):
+        self.N = np.size(initial_prob)
+        self.initial_prob = initial_prob
+        self.trans_prob = trans_prob
+        self.emission = tf.constant(obs_prob)
 
 formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
 logger = logging.getLogger('main')
