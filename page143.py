8718756e71946ba309252fe07f4c06e78148acf4
mikedelong <mikedelong@outlook.com>
2018-03-12 14:11:31 -0400

    feature: autoencoder __init__ plus setting the random seed.

--- page143.py
+++ page143.py
@@ -1,7 +1,37 @@
 import logging
 import time
 
+import tensorflow as tf
+
 start_time = time.time()
+
+random_seed = 143
+
+tf.set_random_seed(random_seed)
+
+
+class Autoencoder:
+    def __init__(self, input_dim, hidden_dim, epoch=250, learning_rate=0.001):
+        self.epoch = epoch
+        self.learning_rate = learning_rate
+        x = tf.placeholder(dtype=tf.float32, shape=[None, input_dim])
+        with tf.name_scope('encode'):
+            weights = tf.Variable(tf.random_normal([input_dim, hidden_dim], dtype=tf.float32, name='weights'))
+            biases = tf.Variable(tf.zeros([hidden_dim]), name='biases')
+            encoded = tf.nn.tanh(tf.matmul(x, weights) + biases)
+
+        with tf.name_scope('decode'):
+            weights = tf.Variable(tf.random_normal([hidden_dim, input_dim], dtype=tf.float32), name='weights')
+            biases = tf.Variable(tf.zeros([input_dim]), name='biases')
+            decoded = tf.matmul(encoded, weights) + biases
+
+        self.x = x
+        self.encoded = encoded
+        self.decoded = decoded
+
+        self.loss = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(self.x, self.decoded))))
+        self.training_operation = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
+        self.saver = tf.train.Saver()
 
 formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
 logger = logging.getLogger('main')
