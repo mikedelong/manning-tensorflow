2f473b4b9e125096bacff0e21e4904f318a33d25
mikedelong <mikedelong@outlook.com>
2018-03-17 16:28:13 -0400

    refactor: moved the main (stub) to __main__

--- page143.py
+++ page143.py
@@ -56,18 +56,18 @@ class Autoencoder:
         return reconstructed
 
 
+if __name__ == '__main__':
+    formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    console_handler.setLevel(logging.DEBUG)
+    logger.debug('started')
 
-formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
-logger = logging.getLogger('main')
-logger.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
-console_handler.setLevel(logging.DEBUG)
-logger.debug('started')
-
-logger.debug('done')
-finish_time = time.time()
-elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
-elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
-logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    logger.debug('done')
+    finish_time = time.time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
