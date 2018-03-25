4d02cf01cc4b802ed0424f8d2154fc685d77e56e
Mike DeLong <mikedelong@outlook.com>
2018-03-25 11:56:18 -0400

    feature: get_prices(); untested.

--- page160.py
+++ page160.py
@@ -1,7 +1,22 @@
 import logging
 import time
 
+import numpy as np
+from yahoo_finance import Share
+
 start_time = time.time()
+
+
+def get_prices(share_symbol, start_date, end_date, cache_filename='stock_prices.npy'):
+    try:
+        stock_prices = np.load(cache_filename)
+    except IOError:
+        share = Share(share_symbol)
+        stock_hist = share.get_historical(start_date=start_date, end_date=end_date)
+        stock_prices = [stock_price['Open'] for stock_price in stock_hist]
+        np.save(cache_filename, stock_prices)
+
+
 
 if __name__ == '__main__':
     formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
