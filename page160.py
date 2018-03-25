cd4db08c2d068f4e9179eb98b29bb74c963b04ca
Mike DeLong <mikedelong@outlook.com>
2018-03-25 12:16:03 -0400

    feature: added the BeautifulSoup-based workaround; need to integrate it.

--- page160.py
+++ page160.py
@@ -1,9 +1,25 @@
 import logging
 import time
+import urllib2
 
+import bs4
 import matplotlib.pyplot as plt
 import numpy as np
 from yahoo_finance import Share
+
+
+def get_historical_data(name, number_of_days):
+    data = []
+    url = "https://finance.yahoo.com/quote/" + name + "/history/"
+    rows = bs4(urllib2.urlopen(url).read()).findAll('table')[0].tbody.findAll('tr')
+
+    for each_row in rows:
+        divs = each_row.findAll('td')
+        if divs[1].span.text != 'Dividend':  # Ignore this row in the table
+            # I'm only interested in 'Open' price; For other values, play with divs[1 - 5]
+            data.append({'Date': divs[0].span.text, 'Open': float(divs[1].span.text.replace(',', ''))})
+
+    return data[:number_of_days]
 
 start_time = time.time()
 
@@ -38,6 +54,9 @@ if __name__ == '__main__':
     logger.addHandler(console_handler)
     console_handler.setLevel(logging.DEBUG)
     logger.debug('started')
+
+    prices = get_prices('MSFT', '1992-07-22', '2016-07-22')
+    plot_prices(prices)
 
     logger.debug('done')
     finish_time = time.time()
