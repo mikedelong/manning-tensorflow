60137cc2264ba4c23b60233a13c557254fac33aa
Mike DeLong <mikedelong@outlook.com>
2018-03-25 12:04:01 -0400

    feature: plot_prices(); untested.

--- page160.py
+++ page160.py
@@ -1,6 +1,7 @@
 import logging
 import time
 
+import matplotlib.pyplot as plt
 import numpy as np
 from yahoo_finance import Share
 
@@ -15,6 +16,16 @@ def get_prices(share_symbol, start_date, end_date, cache_filename='stock_prices.
         stock_hist = share.get_historical(start_date=start_date, end_date=end_date)
         stock_prices = [stock_price['Open'] for stock_price in stock_hist]
         np.save(cache_filename, stock_prices)
+    result = stock_prices.astype(float)
+    return result
+
+
+def plot_prices(prices):
+    plt.title('Opening stock prices')
+    plt.xlabel('day')
+    plt.ylabel('price ($)')
+    plt.plot(prices)
+    plt.savefig('./output/prices.png')
 
 
 
