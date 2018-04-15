125f8c9fea2b048252e80ea156537df8df4697de
mikedelong <mikedelong@outlook.com>
2018-04-15 16:59:26 -0400

    feature: log the output file for the convolution results

--- page174a.py
+++ page174a.py
@@ -132,9 +132,9 @@ with tf.Session() as sess:
     show_weights(W_val, full_output_file)
 
     conv_val = sess.run(conv)
-    logger.debug('convolution results: %s' % str(np.shape(conv_val)))
     output_file = 'page174-convolution-results.png'
     full_output_file = output_folder + output_file
+    logger.debug('convolution results: %s saved to %s' % (str(np.shape(conv_val)), full_output_file))
     show_conv_results(conv_val, full_output_file)
 
     conv_out_val = sess.run(conv_out)
