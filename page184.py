3787fafbd9fb29f31fef248ff0b92092a4b87d74
mikedelong <mikedelong@outlook.com>
2018-04-16 16:00:51 -0400

    feature: shell code with logging, timing, and basic tensorflow initialization.

---
+++ page184.py
@@ -0,0 +1,27 @@
+import logging
+import time
+
+import tensorflow as tf
+
+start_time = time.time()
+
+if __name__ == '__main__':
+    formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    console_handler.setLevel(logging.DEBUG)
+    logger.debug('started')
+
+    with tf.Session() as session:
+        session.run(tf.global_variables_initializer())
+
+    logger.debug('done')
+    finish_time = time.time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    console_handler.close()
+    logger.removeHandler(console_handler)
