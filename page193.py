b8b22fcb3a7a2ea7928773a46f8943ba8bca93f7
mikedelong <mikedelong@outlook.com>
2018-05-12 09:25:37 -0400

    feature: the SeriesPredictor class constructor and model.

--- page193.py
+++ page193.py
@@ -1,6 +1,35 @@
 import logging
 import time
 
+import tensorflow as tf
+from tensorflow.contrib import rnn
+
+
+class SeriesPredictor:
+    def __init__(self, input_dim, seq_size, hidden_dim=10):
+        self.input_dim = input_dim
+        self.seq_size = seq_size
+        self.hidden_dim = hidden_dim
+
+        self.W_out = tf.Variable(tf.random_normal([hidden_dim, 1]), name='W_out')
+        self.b_out = tf.Variable(tf.random_normal([1]), name='b_out')
+        self.x = tf.placeholder(tf.float32, [None, seq_size, input_dim])
+        self.y = tf.placeholder(tf.float32, [None, seq_size])
+
+        self.cost = tf.reduce_mean(tf.square(self.model() - self.y))
+        self.train_op = tf.train.AdamOptimizer().minimize(self.cost)
+
+        self.saver = tf.train.Saver()
+
+    def model(self):
+        cell = rnn.BasicLSTMCell(self.hidden_dim)
+        outputs, states = tf.nn.dynamic_rnn(cell, self.x, dtype=tf.float32)
+        num_examples = tf.shape(self.x)[0]
+        W_repeated = tf.tile(tf.expand_dims(self.W_out, 0), [num_examples, 1, 1])
+        result = tf.matmul(outputs, W_repeated) + self.b_out
+        result = tf.squeeze(result)
+        return result
+
 if __name__ == '__main__':
     start_time = time.time()
 
