9cfd92359444dcd8436368dc1b05ce67cecf78cb
mikedelong <mikedelong@outlook.com>
2018-05-14 08:58:49 -0400

    new file: test code for the series predictor

---
+++ page195.py
@@ -0,0 +1,29 @@
+import logging
+import time
+
+from page193 import SeriesPredictor
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    console_handler.setLevel(logging.DEBUG)
+    logger.debug('started')
+
+    predictor = SeriesPredictor(input_dim=1, seq_size=4, hidden_dim=10)
+    train_x = [[[1], [2], [5], [6]], [[5], [7], [7], [8]], [[3], [4], [5], [8]]]
+    train_y = [[1, 3, 7, 11], [5, 12, 14, 15], [3, 7, 9, 12]]
+    predictor.train(train_x=train_x, train_y=train_y)
+
+    logger.debug('done')
+    finish_time = time.time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    console_handler.close()
+    logger.removeHandler(console_handler)
