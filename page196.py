256c85e489b5c7e70c2f90a95571624870824cea
mikedelong <mikedelong@outlook.com>
2018-05-20 11:03:19 -0400

    feature: load_series and split_data functions.

---
+++ page196.py
@@ -0,0 +1,43 @@
+import csv
+import logging
+import time
+
+import numpy as np
+
+
+def load_series(filename, series_idx=1):
+    try:
+        with open(filename) as csvfile:
+            csvreader = csv.reader(csvfile)
+            data = [float(row[series_idx]) for row in csvreader if len(row) > 0]
+            normalized_data = (data - np.mean(data)) / np.std(data)
+            return normalized_data
+    except IOError:
+        # todo log this
+        return None
+
+
+def split_data(data, percent_train=0.8):
+    num_rows = len(data) * percent_train
+    return data[:num_rows], data[num_rows:]
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    console_handler.setLevel(logging.DEBUG)
+    logger.debug('started')
+
+    logger.debug('done')
+    finish_time = time.time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    console_handler.close()
+    logger.removeHandler(console_handler)
