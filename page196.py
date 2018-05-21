06af4d2e3528f5150442f743b3d8595c3cd8f01b
mikedelong <mikedelong@outlook.com>
2018-05-21 12:27:58 -0400

    feature: add a logger so we can issue a warning if the file load fails. todo: remove the TODO associated with the change above.

--- page196.py
+++ page196.py
@@ -5,15 +5,15 @@ import time
 import numpy as np
 
 
-def load_series(filename, series_idx=1):
+def load_series(filename, series_idx, arg_logger):
     try:
         with open(filename) as csvfile:
             csvreader = csv.reader(csvfile)
             data = [float(row[series_idx]) for row in csvreader if len(row) > 0]
             normalized_data = (data - np.mean(data)) / np.std(data)
             return normalized_data
-    except IOError:
-        # todo log this
+    except IOError as ioError:
+        arg_logger.warning(ioError)
         return None
 
 
