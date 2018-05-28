0c0993c392ddfe616ee0ac854044c39a24319621
mikedelong <mikedelong@outlook.com>
2018-05-28 12:25:55 -0400

    cleanup: code the code back to just a simple test

--- page213.py
+++ page213.py
@@ -2,9 +2,7 @@ import logging
 import time
 
 import tensorflow as tf
-import tensorflow.contrib.layers as layers
 import tensorflow.contrib.rnn as rnn
-import tensorflow.contrib.seq2seq as seq2seq
 
 import page206
 
@@ -42,67 +40,6 @@ if __name__ == '__main__':
     logger.debug(input_symbol_to_int)
     logger.debug(output_int_to_symbol)
     logger.debug(output_symbol_to_int)
-
-    epoch_count = 300
-    rnn_state_dimension = 512
-    rnn_layer_count = 2
-    encoder_embedding_dimension = 64
-    decoder_embedding_dimension = 64
-    batch_size = int(32)
-    learning_rate = 3e-4
-    input_vocabulary_size = len(input_symbol_to_int)
-    output_vocabulary_size = len(output_symbol_to_int)
-
-    encoder_input_sequence = tf.placeholder(tf.int32, [None, None], name='encoder_input_sequence')
-    encoder_sequence_length = tf.placeholder(tf.int32, (None,), name='encoder_sequence_length')
-    decoder_output_sequence = tf.placeholder(tf.int32, [None, None], name='encoder_output_sequence')
-    decoder_sequence_length = tf.placeholder(tf.int32, (None,), name='decoder_sequence_length')
-    max_decoder_sequence_length = tf.reduce_max(decoder_sequence_length, name='max_decoder_sequence_length')
-
-    encoder_input_embedded = layers.embed_sequence(encoder_input_sequence, input_vocabulary_size,
-                                                   encoder_embedding_dimension)
-    encoder_multi_cell = make_multi_cell(rnn_state_dimension, rnn_layer_count)
-    encoder_output, encoder_state = tf.nn.dynamic_rnn(encoder_multi_cell, encoder_input_embedded,
-                                                      sequence_length=encoder_sequence_length, dtype=tf.float32)
-
-    del encoder_output
-
-    decoder_raw_sequence = decoder_output_sequence[:, :-1]
-    go_prefixes = tf.fill([batch_size, 1], output_symbol_to_int['<GO>'])
-    decoder_input_sequence = tf.concat([go_prefixes, decoder_raw_sequence], 1)
-
-    decoder_embedding = tf.Variable(tf.random_uniform([output_vocabulary_size, decoder_embedding_dimension]))
-    decoder_input_embedded = tf.nn.embedding_lookup(decoder_embedding, decoder_input_sequence)
-    decoder_multi_cell = make_multi_cell(rnn_state_dimension, rnn_layer_count)
-    output_layer_kernel_initializer = tf.truncated_normal_initializer(mean=0.0, stddev=1.0)
-    output_layer = tf.layers.Dense(output_vocabulary_size, kernel_initializer=output_layer_kernel_initializer)
-
-    with tf.variable_scope('decode'):
-        training_helper = seq2seq.TrainingHelper(inputs=decoder_input_embedded,
-                                                 sequence_length=decoder_sequence_length, time_major=False)
-        training_decoder = seq2seq.BasicDecoder(decoder_multi_cell, training_helper, encoder_state, output_layer)
-        training_decoder_output_sequence, _, _ = seq2seq.dynamic_decode(training_decoder, impute_finished=True,
-                                                                        maximum_iterations=max_decoder_sequence_length)
-
-    with tf.variable_scope('decode', reuse=True):
-        start_tokens = tf.tile(tf.constant([output_symbol_to_int['<GO>']], dtype=tf.int32), [batch_size],
-                               name='start_tokens')
-        inference_helper = seq2seq.GreedyEmbeddingHelper(embedding=decoder_embedding, start_tokens=start_tokens,
-                                                         end_token=output_symbol_to_int['<EOS>'])
-        inference_decoder = seq2seq.BasicDecoder(decoder_multi_cell, inference_helper, encoder_state, output_layer)
-        inference_decoder_output_sequence, _, _ = seq2seq.dynamic_decode(inference_decoder, impute_finished=True,
-                                                                         maximum_iterations=max_decoder_sequence_length)
-
-    training_logits = tf.identity(training_decoder_output_sequence.rnn_output, name='logits')
-    inference_logits = tf.identity(inference_decoder_output_sequence.sample_id, name='predictions')
-    masks = tf.sequence_mask(decoder_sequence_length, max_decoder_sequence_length, dtype=tf.float32, name='masks')
-    cost = seq2seq.sequence_loss(training_logits, decoder_output_sequence, masks)
-
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-    gradients = optimizer.compute_gradients(cost)
-    capped_gradients = [(tf.clip_by_value(grad, -5.0, 5.0), var) for grad, var in gradients if grad is not None]
-    train_operation = optimizer.apply_gradients(capped_gradients)
-
 
     logger.debug('done')
     finish_time = time.time()
