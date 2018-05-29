b616dec2fba3b06da42f79570898fa253b3b356b
mikedelong <mikedelong@outlook.com>
2018-05-29 09:29:01 -0400

    feature: added an output file to write the scatter plot feature: add safety code so we can either stop and show or we can write to a file feature: disambiguate function-scope and global-scope variables

--- page227.py
+++ page227.py
@@ -5,13 +5,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def get_data(arg_feature_count):
-    data_a = np.random.rand(10, arg_feature_count) + 1
-    data_b = np.random.rand(10, arg_feature_count)
-    plt.scatter(data_a[:, 0], data_a[:, 1], c='r', marker='x')
-    plt.scatter(data_b[:, 0], data_b[:, 1], c='g', marker='o')
-    plt.show()
-    return data_a, data_b
+def get_data(arg_feature_count, arg_output_file=None):
+    result_a = np.random.rand(10, arg_feature_count) + 1
+    result_b = np.random.rand(10, arg_feature_count)
+    plt.scatter(result_a[:, 0], result_a[:, 1], c='r', marker='x')
+    plt.scatter(result_b[:, 0], result_b[:, 1], c='g', marker='o')
+    if arg_output_file is None:
+        plt.show()
+    else:
+        plt.savefig(arg_output_file)
+    return result_a, result_b
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -28,7 +31,9 @@ if __name__ == '__main__':
     feature_count = 2
     random_seed = 1
     np.random.seed(random_seed)
-    data_a, data_b = get_data(arg_feature_count=feature_count)
+    scatter_plot_file = './output/page227-scatter.png'
+    logger.debug('writing scatter plot to %s' % scatter_plot_file)
+    data_a, data_b = get_data(arg_feature_count=feature_count, arg_output_file=scatter_plot_file)
 
     logger.debug('done')
     finish_time = time.time()
