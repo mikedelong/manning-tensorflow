e7ca203e5e7fe893e4129b2c6e1ea00a7f0f5bbc
mikedelong <mikedelong@outlook.com>
2018-05-29 09:25:41 -0400

    feature: get_data() and some code to exercise it

--- page227.py
+++ page227.py
@@ -1,6 +1,18 @@
 import logging
 import time
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_data(arg_feature_count):
+    data_a = np.random.rand(10, arg_feature_count) + 1
+    data_b = np.random.rand(10, arg_feature_count)
+    plt.scatter(data_a[:, 0], data_a[:, 1], c='r', marker='x')
+    plt.scatter(data_b[:, 0], data_b[:, 1], c='g', marker='o')
+    plt.show()
+    return data_a, data_b
+
 if __name__ == '__main__':
     start_time = time.time()
 
@@ -12,6 +24,11 @@ if __name__ == '__main__':
     logger.addHandler(console_handler)
     console_handler.setLevel(logging.DEBUG)
     logger.debug('started')
+
+    feature_count = 2
+    random_seed = 1
+    np.random.seed(random_seed)
+    data_a, data_b = get_data(arg_feature_count=feature_count)
 
     logger.debug('done')
     finish_time = time.time()
