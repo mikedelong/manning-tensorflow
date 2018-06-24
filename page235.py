0f3f6082b1cdb02aec386d27ebbfe94089fce3a5
mikedelong <mikedelong@outlook.com>
2018-06-24 17:46:47 -0400

    feature: prepare the data for ranking

--- page235.py
+++ page235.py
@@ -4,8 +4,11 @@ import os
 import time
 
 import numpy as np
+import tensorflow as tf
 from scipy.misc import imread
 from scipy.misc import imresize
+
+from vgg16 import vgg16
 
 DATASET_DIR = os.path.join(os.path.expanduser('-'), 'res', 'cloth_folding_rgb_vids')
 NUM_VIDS = 45
@@ -50,6 +53,22 @@ if __name__ == '__main__':
     logger.debug('loading VGG16 model')
     vgg = vgg16(images_placeholder, 'vgg16_weights.npz', session)
     logger.debug('done loading the VGG16 model.')
+
+    start_images_embedded = session.run(vgg.fc1, feed_dict={vgg.imgs: start_images})
+    end_images_embedded = session.run(vgg.fc1, feed_dict={vgg.imgs: end_images})
+    indexes = np.random.choice(NUM_VIDS, NUM_VIDS, replace=False)
+    train_indexes = indexes[0:int(NUM_VIDS * 0.75)]
+    test_indexes = indexes[int(NUM_VIDS * 0.75):]
+
+    train_start_images = start_images_embedded[train_indexes]
+    train_end_images = end_images_embedded[train_indexes]
+    test_start_images = start_images_embedded[test_indexes]
+    test_end_images = end_images_embedded[test_indexes]
+
+    logger.debug('Train start images {}'.format(np.shape(train_start_images)))
+    logger.debug('Train end images {}'.format(np.shape(train_end_images)))
+    logger.debug('Test start images {}'.format(np.shape(test_start_images)))
+    logger.debug('Test end images {}'.format(np.shape(test_end_images)))
 
     logger.debug('done')
     finish_time = time.time()
