e490c81b955332cd99f47b4fcd912054be475bd9
mikedelong <mikedelong@outlook.com>
2018-06-24 17:57:53 -0400

    feature: introduce code from the simpler example feature: train the ranking network

--- page235.py
+++ page235.py
@@ -48,6 +48,47 @@ if __name__ == '__main__':
     logger.debug('Images of ending state {}'.format(np.shape(end_images)))
 
     images_placeholder = tf.placeholder(tf.float32, [None, 224, 224, 3])
+    feature_count = 2
+    hidden_count = 10
+
+    with tf.name_scope('input'):
+        x1 = tf.placeholder(tf.float32, [None, feature_count], name='x1')
+        x2 = tf.placeholder(tf.float32, [None, feature_count], name='x2')
+        dropout_keep_probability = tf.placeholder(tf.float32, name='dropout_probability')
+
+    with tf.name_scope('hidden_layer'):
+        with tf.name_scope('weights'):
+            w1 = tf.Variable(tf.random_normal([feature_count, hidden_count]), name='w1')
+            tf.summary.histogram('w1', w1)
+            b1 = tf.Variable(tf.random_normal([hidden_count]), name='b1')
+            tf.summary.histogram('b1', b1)
+
+        with tf.name_scope('output'):
+            h1 = tf.nn.dropout(tf.nn.relu(tf.matmul(x1, w1) + b1), keep_prob=dropout_keep_probability)
+            tf.summary.histogram('h1', h1)
+            h2 = tf.nn.dropout(tf.nn.relu(tf.matmul(x2, w1) + b1), keep_prob=dropout_keep_probability)
+            tf.summary.histogram('h2', h2)
+
+    with tf.name_scope('output_layer'):
+        with tf.name_scope('output_layer'):
+            w2 = tf.Variable(tf.random_normal([hidden_count, 1]), name='w2')
+            tf.summary.histogram('w2', w2)
+            b2 = tf.Variable(tf.random_normal([1]), name='b2')
+            tf.summary.histogram('b2', b2)
+        with tf.name_scope('output'):
+            s1 = tf.matmul(h1, w2) + b2
+            s2 = tf.matmul(h2, w2) + b2
+
+    with tf.name_scope('loss'):
+        s12 = s1 - s2
+        s12_flat = tf.reshape(s12, [-1])
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.zeros_like(s12_flat), logits=s12_flat + 1)
+        loss = tf.reduce_mean(cross_entropy)
+
+    with tf.name_scope('train_op'):
+        train_operation = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+
+
     session = tf.InteractiveSession()
     session.run(tf.global_variables_initializer())
     logger.debug('loading VGG16 model')
@@ -69,6 +110,20 @@ if __name__ == '__main__':
     logger.debug('Train end images {}'.format(np.shape(train_end_images)))
     logger.debug('Test start images {}'.format(np.shape(test_start_images)))
     logger.debug('Test end images {}'.format(np.shape(test_end_images)))
+
+    train_y1 = np.expand_dims(np.zeros(np.shape(train_start_images)[0]), axis=1)
+    train_y2 = np.expand_dims(np.ones(np.shape(train_end_images)[0]), axis=1)
+    for epoch in range(100):
+        for index in range(np.shape(train_start_images)[0]):
+            _, cost_value = session.run([train_operation, loss], feed_dict={x1: train_start_images[index:index + 1, :],
+                                                                            x2: train_end_images[index:index + 1, :],
+                                                                            dropout_keep_probability: 0.5})
+            logger.debug('{} {}'.format(epoch, cost_value))
+            s1_value, s2_value = session.run([s1, s2], feed_dict={x1: test_start_images,
+                                                                  x2: test_end_images,
+                                                                  dropout_keep_probability: 1.0})
+            logger.debug('Accuracy: {}%'.format(100 * np.mean(s1_value < s2_value)))
+
 
     logger.debug('done')
     finish_time = time.time()
